5724e028a54951e3b0eb92203256d92d5e1c0b86
unknown <Junior@JuniorPC.(none)>
2012-07-21 16:40:31 -0500

    init

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ var express = require('express')
   , routes = require('./routes')
   , http = require('http');
 
-var app = express();
+var app = module.exports = express.createServer();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  //app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
@@ -27,6 +27,26 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+//http.createServer(app).listen(app.get('port'), function(){
+//  console.log("Express server listening on port " + app.get('port'));
+//});
+
+
+var io = require('socket.io').listen(app);
+
+// assuming io is the Socket.IO server object
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
 });
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+app.listen('port', process.env.PORT || 3000);
+//console.log("Nodejs blackboard listening on port %d in %s mode", app.address().port, app.settings.env);
+
